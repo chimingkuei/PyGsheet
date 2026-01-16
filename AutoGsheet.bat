11f3e9b49e21937214d835cc5f5d3d0c069241b0
chimingkuei <chimingkuei@gmail.com>
2026-01-16 15:16:07 +0800

    AutoGsheet

--- AutoGsheet.bat
+++ AutoGsheet.bat
@@ -1,4 +1,5 @@
 @echo off
 color 0b
+call activate gspread_env
 python AutoGsheet.py
 pause
--- AutoGsheet.py
+++ AutoGsheet.py
@@ -1,8 +1,20 @@
-import gspread
+# import gspread
 
-share_link = "https://docs.google.com/spreadsheets/d/1k2yjKUA4m886_Vj24V_jnpt4ClTkQIjoabBBQKb4Jko/edit"
-gc = gspread.service_account(filename='token.json')
-worksheet = gc.open_by_url(share_link).worksheet('01.06(二)')
-for v in worksheet.col_values(3)[4:]:
-    print(v)
+# share_link = "https://docs.google.com/spreadsheets/d/1k2yjKUA4m886_Vj24V_jnpt4ClTkQIjoabBBQKb4Jko/edit"
+# gc = gspread.service_account(filename='token.json')
+# worksheet = gc.open_by_url(share_link).sheet1
+# for v in worksheet.col_values(3)[4:]:
+#     print(v)
 
+import schedule
+import time
+
+def job():
+    print("hello")
+
+# schedule 預設最小單位是秒，所以可以用 every(3).seconds
+schedule.every(3).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # 避免 CPU 100%
